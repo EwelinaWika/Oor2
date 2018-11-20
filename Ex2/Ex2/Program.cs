b0f412c2d17e6387118e6823ef85eb9244dbe824
Ewelina <ewelina-wika@wp.pl>
2018-11-20 04:14:19 +0100

    1

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static readonly object _object = new object();
+    static int v = 0;
+   
+    static void Main()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            new Thread(Locks).Start();
+        }
+        new Thread(() =>
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Wartość: " + v);
+        }).Start();
+        Console.ReadKey();
+    }
+    static void Locks()
+    {
+        lock (_object)
+        {
+            v += 10;
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ Ex22/Ex22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static readonly object _object = new object();
+    static int v = 0;
+
+    static void Main()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            new Thread(Locks).Start();
+        }
+        new Thread(() =>
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Wartość: " + v);
+        }).Start();
+        Console.ReadKey();
+    }
+    static void Locks()
+    {
+        
+            v += 10;
+            Thread.Sleep(10000);
+       
+    }
+}
